08c63844d5a54dee869dcfbd585b24dd03d8e2d5
ffu <ffu@sma-partner.com>
2020-02-05 19:54:07 +0100

    Restructred Tests

--- test_interface_with_viriato.py
+++ test_interface_with_viriato.py
@@ -14,34 +14,48 @@ def test_object_initialisiation(url_str='http://localhost:8080/'):
         # if we made it here, there is something wrong
         print('was able to insert int as url')
         return []
-    except(AssertionError):
+    except AssertionError:
         print('failed corretly to insert int as url')
         # create an interface with the url as str
         interface_to_viriato = interface_module.AlgorithmicPlatformInterface(url_str)
         return interface_to_viriato
 
 
+def test_user_notifications(interface_to_viriato):
+    # test user notifications:
+    interface_to_viriato.notify_user('hi', 'it works')
+    print('notify user test complete')
+    try:
+        for i in range(99):
+            interface_to_viriato.show_status_message(i)
+        print('was able to insert int as show_status_message')
+        return 1
+    except AssertionError:
+        print('failed correctly to insert int as show_status_message')
+        # create an interface with the url as str
+        interface_to_viriato.show_status_message('Foo', 'bar')
+        interface_to_viriato.show_status_message('Foo bar')
+    print('show_status_message test complete')
+    return 0
+
+
 def main():
     interface_to_viriato = test_object_initialisiation()
-    interface_to_viriato.verbosity = 1  # increase verbosity
+    interface_to_viriato.verbosity = 0  # increase verbosity
 
     # try to retrieve the url:
     print(interface_to_viriato.retrieve_url_to_port())
     print('url retrive test complete')
 
-    # test user notifications:
-    interface_to_viriato.notify_user('hi', 'it works')
-    print('notify user test complete')
-    interface_to_viriato.show_status_message('hi', 'it works')
-    for i in range(99):
-        interface_to_viriato.show_status_message(i)
-    print('show_status_message test complete')
+    check_int = test_user_notifications(interface_to_viriato)
+    if check_int != 0:
+        raise
 
     # this is a depreciated call!
     # test_str = 'parameters/train'
     # response = interface_to_viriato.do_request(test_str, 'GET')
     # print(response.json())
-    return 1
+    return 0
 
 
 if __name__ == '__main__':

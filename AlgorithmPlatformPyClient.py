e12dfa2de8ae871f596a28de4c90c907281a6dc1
ffu <ffu@sma-partner.com>
2020-02-06 08:19:37 +0100

    Cleaned Strings, consistent formatting and no more C style Enhanced get

--- AlgorithmPlatformPyClient.py
+++ AlgorithmPlatformPyClient.py
@@ -20,7 +20,7 @@ class AlgorithmicPlatformInterface:
         :type url_to_port: str
         """
         # to avoid side effects, it is a protected attribute, instantiate a new object if you want to change it
-        assert isinstance(url_to_port, str), "id is not a str : {}".format(url_to_port)
+        assert isinstance(url_to_port, str), 'id is not a str : {0}'.format(url_to_port)
         self.__url_to_port = url_to_port
 
     # helper (private) to check if there is an error --> exception is here
@@ -35,28 +35,33 @@ class AlgorithmicPlatformInterface:
 
     def notify_user(self, message_level_1, message_level_2):
         # bullet proof, check for strings to be sent:
-        assert isinstance(message_level_1, str), "message_level_1 is not a str : {}".format(message_level_1)
-        assert isinstance(message_level_2, str), "message_level_2 is not a str : {}".format(message_level_2)
+        assert isinstance(message_level_1, str), 'message_level_1 is not a str : {0}'.format(message_level_1)
+        assert isinstance(message_level_2, str), 'message_level_2 is not a str : {0}'.format(message_level_2)
         # assemble the dict
         body = dict(messageLevel1=message_level_1, messageLevel2=message_level_2)
-        resp = requests.post(self.__url_to_port + "notifications", json=body)
+        resp = requests.post('{0}notifications'.format(self.__url_to_port), json=body)
         self.__check_status(resp)
 
     def show_status_message(self, short_message, long_message=None):
         # bullet proof, check for strings to be sent:
-        assert isinstance(short_message, str), "short_message is not a str : {}".format(short_message)
+        assert isinstance(short_message, str), 'short_message is not a str : {0}'.format(short_message)
         if not (long_message is None):
-            assert isinstance(long_message, str), "long_message is not a str : {}".format(long_message)
+            assert isinstance(long_message, str), 'long_message is not a str : {0}'.format(long_message)
         # not to sure about the str part!
         body = dict(shortMessage=short_message, longMessage=long_message)
-        resp = requests.post(self.__url_to_port + "status-message", json=body)
+        resp = requests.post('{0}status-message'.format(self.__url_to_port), json=body)
         self.__check_status(resp)
 
     def get_directed_section_tracks(self, first_node_id, second_node_id):
         # bullet proofing
-        assert isinstance(first_node_id, int), "first_node_id is not an int: {}".format(first_node_id)
-        assert isinstance(second_node_id, int), "second_node_id is not an int: {}".format(second_node_id)
+        assert isinstance(first_node_id, int), 'first_node_id is not an int: {0}'.format(first_node_id)
+        assert isinstance(second_node_id, int), 'second_node_id is not an int: {0}'.format(second_node_id)
         # insert method later pls, check assertion first
+        get_request_str = 'assignable-station-tracks-on-train-path-node?'
+        get_request_parameters = dict(firstNodeID=first_node_id, secondNodeID=second_node_id)
+        api_response = requests.get('{0}assignable-station-tracks-on-train-path-node?'.format(self.__url_to_port),
+                                    json=get_request_parameters)
+
 
 
 # this class is only ment as a debug/etc, not for productive use!

4c6cccd7f72d55def56d65a09dbf89c25259df8f
ffu <ffu@sma-partner.com>
2020-02-06 08:03:52 +0100

    removed float from accepted objects as it is wrong

--- AlgorithmPlatformPyClient.py
+++ AlgorithmPlatformPyClient.py
@@ -53,8 +53,8 @@ class AlgorithmicPlatformInterface:
 
     def get_directed_section_tracks(self, first_node_id, second_node_id):
         # bullet proofing
-        assert isinstance(first_node_id, (float, int)),
-        assert isinstance(second_node_id, (float, int))
+        assert isinstance(first_node_id, int), "first_node_id is not an int: {}" .format(first_node_id)
+        assert isinstance(second_node_id, int), "second_node_id is not an int: {}" .format(second_node_id)
         # insert method later pls, check assertion first
 
 # this class is only ment as a debug/etc, not for productive use!

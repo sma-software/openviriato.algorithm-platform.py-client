01175e19ed8d5ba6c714502c7f8a3c9d5e8d198c
ffu <ffu@sma-partner.com>
2020-02-06 09:13:10 +0100

    testing get directed section tracks

--- test_interface_with_viriato.py
+++ test_interface_with_viriato.py
@@ -5,7 +5,7 @@ A test script that requires an REST-API of the VIRIATO-Algorithm Platform
 import AlgorithmPlatformPyClient as interface_module
 
 
-def test_object_initialisiation(url_str='http://localhost:8080/'):
+def test_object_initialisation(url_str='http://localhost:8080/'):
     # test for the object creation:
     # fails on purpose:
     try:
@@ -15,7 +15,7 @@ def test_object_initialisiation(url_str='http://localhost:8080/'):
         print('was able to insert int as url')
         return []
     except AssertionError:
-        print('failed corretly to insert int as url')
+        print('failed to insert int as url')
         # create an interface with the url as str
         interface_to_viriato = interface_module.AlgorithmicPlatformInterface(url_str)
         return interface_to_viriato
@@ -39,10 +39,19 @@ def test_user_notifications(interface_to_viriato):
     return 0
 
 
-def test_get_directed_section_tracks(interface_to_viriato):
+def test_get_directed_section_tracks(interface_to_viriato: ) -> int:
+    """
+    :return: 0 if passed
+    :rtype: int
+    :type interface_to_viriato: interface_module.AlgorithmicPlatformInterface
+    """
+    api_return = interface_to_viriato.get_directed_section_tracks(161, 348)
+    if isinstance(api_return, dict):
+        print(api_return)
+
     # lets test the robustness:
-    for i in range(1, 100):
-        for j in range(1, 100):
+    for i in range(20, 100):
+        for j in range(20, 100):
             if i == j:
                 continue
             api_return = interface_to_viriato.get_directed_section_tracks(i, j)
@@ -51,13 +60,13 @@ def test_get_directed_section_tracks(interface_to_viriato):
 
     return 0
 
-
+#
 def main():
-    interface_to_viriato = test_object_initialisiation()
+    interface_to_viriato = test_object_initialisation()
     interface_to_viriato.verbosity = 1  # increase verbosity
 
     # try to retrieve the url:
-    print(interface_to_viriato.retrieve_url_to_port)
+    print(interface_to_viriato.retrieve_url_to_port())
     print('url retrive test complete')
 
     check_int = test_user_notifications(interface_to_viriato)

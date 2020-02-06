59e2f1295f901aa2bc2183d0457316f063cc6862
ffu <ffu@sma-partner.com>
2020-02-06 18:41:15 +0100

    gathering all static elements

---
+++ AlgorithmShared.py
@@ -0,0 +1,46 @@
+import requests
+
+def verify_parameter_is_str(received_object: object, object_name: str, method_name: str):
+    assert isinstance(received_object, str), \
+        "in method {0}, \n the parameter {0} is required to be of type str \n " \
+        "but it was a instead: {2}".format(method_name, object_name, received_object.__class__)
+
+
+def verify_parameter_is_int(received_object: object, object_name: str, method_name: str):
+    assert isinstance(received_object, int), \
+        "in method {0}, \n the parameter {0} is required to be of type int \n " \
+        "but it was a instead: {2}".format(method_name, object_name, received_object.__class__)
+
+
+def check_if_request_successful(api_return: requests.Response):
+    """
+    not all HTTPError Messages are completely indicative, depends on how the API is configured
+    we therefore display the returned json in an additional error if it is a HTTPError
+    This method is mostly static, could be a function as well.
+    :param api_return: the raw objected returned by the api-request
+    """
+    try:
+        api_return.raise_for_status()
+    except requests.HTTPError:
+        # if there is an error, the algorithm platform supplies us with more information (hopefully)
+        rest_feedback = (api_return.json())
+        raise AlgorithmPlatformError(rest_feedback['statusCode'], rest_feedback['message'])
+    # note that the connection will remain open
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class AlgorithmPlatformError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = 'HTTPError {0}'.format(expression)
+        self.message = message

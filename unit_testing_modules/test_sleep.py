dff0574eb4a652059c19e8a038929e96fcfc5451
ffu <ffu@sma-partner.com>
2020-02-14 14:42:22 +0100

    enabled unit testing

---
+++ unit_testing_modules/test_sleep.py
@@ -0,0 +1,10 @@
+import unittest
+import time
+
+
+class sleepeertest(unittest.TestCase):
+
+    def test_sleep(self):
+        for i in range(3):
+            time.sleep(i)
+            print("slept for {0} seconds".format(i))

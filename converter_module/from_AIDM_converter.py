fea0c50428c00b570180d1da1f90bdf4e08d7d58
ffu <ffu@sma-partner.com>
2020-03-05 16:51:21 +0100

    added unittests for headway methods

--- converter_module/from_AIDM_converter.py
+++ converter_module/from_AIDM_converter.py
@@ -10,13 +10,13 @@ def check_for_datetime(obj):
         obj = isodate.duration_isoformat(obj)
     return obj
 
+
 def convert_to_json_conform_dict(AIDM_object) -> dict:
     attribute_names = [attr for attr in dir(AIDM_object) if not attr.startswith('_')]
     attribute_dict = dict()
     for attribute_name in attribute_names:
         attribute_dict[attribute_name] = check_for_datetime(getattr(AIDM_object, attribute_name))
     return attribute_dict
-
 
 
 def convert_to_list_of_dict(list_of_AIDM_object: list) -> list:

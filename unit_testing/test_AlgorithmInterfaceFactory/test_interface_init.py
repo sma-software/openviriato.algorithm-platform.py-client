b78b7e087b026da0e8f5b6f8e7abb9a414f336a8
ffu <ffu@sma-partner.com>
2020-03-17 10:59:24 +0100

    refactored/-structured tests for interface init

---
+++ unit_testing/test_AlgorithmInterfaceFactory/test_interface_init.py
@@ -0,0 +1,20 @@
+import unittest
+from AlgorithmInterface import AlgorithmInterface, AlgorithmInterfaceFactory
+
+
+class TestObjectInitialisation(unittest.TestCase):
+    def test_initialisation_in_with(self):
+        url_str = 'http://localhost:8080'
+
+        with AlgorithmInterface.AlgorithmicPlatformInterface(url_str) as interface_to_viriato:
+            self.assertEqual(interface_to_viriato.base_url, url_str)
+
+    def test_initialisation_from_factory_in_with(self):
+        url_str = 'http://localhost:8080'
+
+        with AlgorithmInterfaceFactory.create(url_str) as interface_to_viriato:
+            self.assertEqual(interface_to_viriato.base_url, url_str)
+
+
+if __name__ == '__main__':
+    unittest.main()

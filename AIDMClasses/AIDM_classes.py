bacd60cdba4c17a513fcba3a53baf6654ddf3e89
ffu <ffu@sma-partner.com>
2020-03-18 15:16:25 +0100

    removed undesired comments

--- AIDMClasses/AIDM_classes.py
+++ AIDMClasses/AIDM_classes.py
@@ -366,22 +366,3 @@ class OutgoingRoutingEdge(IRoutingEdge):
     def StartNodeTrackID(self) -> int:
         return self.__StartNodeTrackID
 
-
-"""
-class voidAlgorithmTimeWindow:
-    __FromTime: datetime.datetime
-    __ToTime: datetime.datetime
-
-    def __init__(self, FromTime: datetime.datetime, ToTime: datetime.datetime):
-        self.__FromTime = FromTime
-        self.__ToTime = ToTime
-
-    @property
-    def FromTime(self) -> datetime.datetime:
-        return self.__FromTime
-
-    @property
-    def ToTime(self) -> datetime.datetime:
-        return self.__ToTime
-
-"""

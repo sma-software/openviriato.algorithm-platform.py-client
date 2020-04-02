99aefe620b805c56a699ce4de9a1bb565c13c5c3
ffu <ffu@sma-partner.com>
2020-04-02 16:13:11 +0200

    VPLAT-7448 Corrected unittest to use UpdateTrainTimesNode instead of updateTrainTimesNode as input

--- AlgorithmInterface_test/trains/test_update_train_times.py
+++ AlgorithmInterface_test/trains/test_update_train_times.py
@@ -60,7 +60,7 @@ class TestUpdateTrainTimes(unittest.TestCase):
     @mock.patch('requests.Session', side_effect=UpdateTrainTimesTestMockSession)
     def test_update_train_times_request(self, mocked_get_obj):
         train_id = 2060
-        update_train_time_nodes = [AIDM_classes.updateTrainTimesNode(TrainPathNodeId=1332,
+        update_train_time_nodes = [AIDM_classes.UpdateTrainTimesNode(TrainPathNodeID=1332,
                                                                      ArrivalTime=datetime.datetime(2003, 5, 1, 0, 4),
                                                                      DepartureTime=datetime.datetime(2003, 5, 1, 0, 5))]
 
@@ -69,21 +69,26 @@ class TestUpdateTrainTimes(unittest.TestCase):
         session_obj = self.interface_to_viriato._AlgorithmicPlatformInterface__communication_layer.currentSession
         self.assertEqual(session_obj._UpdateTrainTimesTestMockSession__last_request,
                          get_api_url() + '/trains/2060/train-path-nodes')
-        self.assertListEqual(session_obj._UpdateTrainTimesTestMockSession__last_body, [{"TrainPathNodeId": 1332,
-                                                                                        "ArrivalTime": "2003-05-01T00:04:00",
-                                                                                        "DepartureTime": "2003-05-01T00:05:00"}])
+        self.assertListEqual(session_obj._UpdateTrainTimesTestMockSession__last_body, [{"TrainPathNodeID": 1332,
+                                                                                        "ArrivalTime":
+                                                                                            "2003-05-01T00:04:00",
+                                                                                        "DepartureTime":
+                                                                                            "2003-05-01T00:05:00",
+                                                                                        'MinimumRunTime': None,
+                                                                                        'MinimumStopTime': None,
+                                                                                        'StopStatus': None}])
 
     @mock.patch('requests.Session', side_effect=UpdateTrainTimesTestMockSession)
     def test_update_train_times_response(self, mocked_get_obj):
         train_id = 2060
-        update_train_time_nodes = [AIDM_classes.updateTrainTimesNode(TrainPathNodeId=1332,
+        update_train_time_nodes = [AIDM_classes.UpdateTrainTimesNode(TrainPathNodeID=1332,
                                                                      ArrivalTime=datetime.datetime(2003, 5, 1, 0, 4),
                                                                      DepartureTime=datetime.datetime(2003, 5, 1, 0, 5))]
 
         updated_algorithm_train = self.interface_to_viriato.update_train_times(train_id, update_train_time_nodes)
 
         self.assertIsInstance(updated_algorithm_train, AIDM_classes.AlgorithmTrain)
-        self.assertEqual(updated_algorithm_train.DebugString, 'Mocked RVZH_1_1_J03 tt_(G)' )
+        self.assertEqual(updated_algorithm_train.DebugString, 'Mocked RVZH_1_1_J03 tt_(G)')
         self.assertEqual(updated_algorithm_train.ID, 2060)
         self.assertIsInstance(updated_algorithm_train.TrainPathNodes, list)
         self.assertIsInstance(updated_algorithm_train.TrainPathNodes[0], AIDM_classes.TrainPathNode)

e0cc2525b5e33d88e9e4e1d0d542ae7826153f60
ffu <ffu@sma-partner.com>
2020-04-08 10:11:36 +0200

    VPLAT-7453: Added four unit-tests to Conversion_test to ensure that missing/unexpected attribute raises exception.
    (cherry picked from commit b3a06742e58b564de89fcfcc03022bcc329b39b6)

---
+++ Conversion_test/test_inequality_of_REST_json_and_AIDM_object.py
@@ -0,0 +1,40 @@
+import unittest
+import AIDMClasses
+import Conversion.algorithm_platform_json_to_AIDM_converter as algorithm_platform_json_to_AIDM_converter
+
+
+class TestEqualityOfRESTJsonAndAIDMObjectValidated(unittest.TestCase):
+
+    def test_convert_with_AIDM_class_missing_attribute_raises_exception(self):
+        node_track_as_dict_with_missing_code_attribute  = dict(ID=1, DebugString= "CodeIsMissing")
+
+        with self.assertRaises(TypeError, msg="__init__() missing 1 required positional argument: 'Code'"):
+            algorithm_platform_json_to_AIDM_converter.convert(
+                AIDMClasses.AlgorithmNodeTrack,
+                node_track_as_dict_with_missing_code_attribute)
+
+    def test_convert_with_AIDM_class_unexpected_attribute_raises_exception(self):
+        node_track_as_dict_with_additional_attribute = dict(ID=1, Code='2', DebugString='3')
+        node_track_as_dict_with_additional_attribute['ThisIs___AWrongName'] = 'unexpectedAttribute'
+
+        with self.assertRaises(TypeError, msg="__init__() got an unexpected keyword argument 'ThisIs___AWrongName'"):
+            algorithm_platform_json_to_AIDM_converter.convert(
+                AIDMClasses.AlgorithmNodeTrack,
+                node_track_as_dict_with_additional_attribute)
+
+    def test_convert_with_AIDM_factory_missing_attribute_raises_exception(self):
+        algorithm_node_with_missing_code_attribute = dict(ID=15, DebugString='test123', NodeTracks=[])
+
+        with self.assertRaises(TypeError, msg="__init__() missing 1 required positional argument: 'Code'"):
+            algorithm_platform_json_to_AIDM_converter.convert(
+                algorithm_platform_json_to_AIDM_converter.convert_json_to_AlgorithmNode,
+                algorithm_node_with_missing_code_attribute)
+
+    def test_convert_with_AIDM_factory_unexpected_attribute_raises_exception(self):
+        algorithm_node_with_additional_attribute = dict(ID=15, Code='A', DebugString='test123', NodeTracks=[])
+        algorithm_node_with_additional_attribute['__A__WrongName'] = 'unexpectedAttribute'
+
+        with self.assertRaises(TypeError, msg="__init__() got an unexpected keyword argument '__A__WrongName'"):
+            algorithm_platform_json_to_AIDM_converter.convert(
+                algorithm_platform_json_to_AIDM_converter.convert_json_to_AlgorithmNode,
+                algorithm_node_with_additional_attribute)

0f38b809b1ceb6e9c0c564513e880ea80e751011
ffu <ffu@sma-partner.com>
2020-07-15 09:57:33 +0200

    VPLAT 7605: Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from setuptools import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -12,9 +11,9 @@ setup(
               'py_client.Conversion',
               'py_client.Communication',
               'py_client.algorithm_interface'],
-    url='https://www.sma-partner.com/en/19-software-en/1401-algorithm-platform',
-    license='Only for authorized use!',
-    author='mhe, ffu',
-    author_email='mhe@sma-partner.com',
+    url='https://www.sma-partner.com',
+    license='',
+    author='',
+    author_email='',
     description=''
 )

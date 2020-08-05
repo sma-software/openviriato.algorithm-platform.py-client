a192db4075e2bc9a996d3dba605b5a6b84a355d2
ffu <ffu@sma-partner.com>
2020-08-05 13:32:52 +0200

    VPLAT-7605: Enhance packaging script to allow packaging outside of IDE

--- jenkins/packaging/packaging_main.py
+++ jenkins/packaging/packaging_main.py
@@ -1,6 +1,10 @@
-from jenkins.packaging.packaging_manager import PackagingManager
-from jenkins.packaging.packaging_arguments import assemble_packaging_arguments
 import sys
+import os
+
+
+def add_project_root_directory_to_python_path(project_root_directory):
+    # python requires the root directory on its path variables to discover the modules/packages in the project
+    sys.path.append(os.path.abspath(project_root_directory))
 
 
 def main():
@@ -14,10 +18,14 @@ def main():
             file=sys.stderr)
         sys.exit(1)
 
-    py_client_build_number = sys.argv[1]
-    name_of_requirements = sys.argv[2]
-    project_root_directory = sys.argv[3]
-    output_directory = sys.argv[4]
+    # since this script is not located in the project root-directory, which is expected by setup-tools we have to add
+    # the project root explicitly
+    add_project_root_directory_to_python_path(project_root_directory=sys.argv[3])
+    # now that we added the root-project-directory to the python path we finally can import elements from jenkins
+    # package
+    from jenkins.packaging.setup_tools_runner import PackagingManager
+    from jenkins.packaging.setup_tools_arguments import SetupToolsArguments
+
 
     packaging_parameters = assemble_packaging_arguments(name_of_requirements, py_client_build_number, output_directory)
     PackagingManager.create_and_start_packaging_manager(packaging_parameters, project_root_directory)

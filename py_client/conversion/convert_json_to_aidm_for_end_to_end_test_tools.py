49007b19e55c09d2a9fdcf8477eaeab1049b0375
ffu <ffu@sma-partner.com>
2020-08-19 14:17:52 +0200

    VPLAT-7612: Complete 04_Timetable Updates test cases with data and required conversion method

--- py_client/conversion/convert_json_to_aidm_for_end_to_end_test_tools.py
+++ py_client/conversion/convert_json_to_aidm_for_end_to_end_test_tools.py
@@ -1,18 +1,27 @@
-from py_client.aidm.aidm_update_classes import UpdateTrainTimesNode
+from py_client.aidm.aidm_enum_classes import StopStatus
+from py_client.aidm.aidm_update_classes import UpdateTrainTimesNode, UpdateTrainStopTimesNode
 from py_client.conversion.converter_helpers import convert_keys_to_snake_case, parse_to_datetime
 from py_client.conversion.algorithm_platform_json_to_aidm_converter import convert
 from typing import List
 
 
 def convert_list_of_json_to_update_train_times_node(attribute_dict: dict) -> List[UpdateTrainTimesNode]:
-    return [convert_json_to_update_train_times_node(node_attributes) for node_attributes in attribute_dict[
-        "train_path_nodes"]]
+    return [convert_json_to_update_train_times_node(node_attributes)
+            for node_attributes in attribute_dict["train_path_nodes"]]
 
 
 def convert_json_to_update_train_times_node(attribute_dict: dict) -> UpdateTrainTimesNode:
     snake_case_dict = convert_keys_to_snake_case(attribute_dict)
     for key in ['arrival_time', 'departure_time']:
         snake_case_dict[key] = parse_to_datetime(snake_case_dict[key])
+    return convert(UpdateTrainTimesNode, snake_case_dict)
+
+
+def convert_json_to_update_train_stop_times_node(attribute: dict) -> UpdateTrainStopTimesNode:
+    snake_case_dict = convert_keys_to_snake_case(attribute)
+    for key in ['arrival_time', 'departure_time']:
+        snake_case_dict[key] = parse_to_datetime(snake_case_dict[key])
+    snake_case_dict["stop_status"] = StopStatus[snake_case_dict["stop_status"]]
     return convert(UpdateTrainTimesNode, snake_case_dict)
 
 

61852541bafeeb3faba95772578c412410734a49
ffu <ffu@sma-partner.com>
2020-09-22 11:02:56 +0200

    VPLAT-7409: Added data and custom converters for 06_Headway Times and Separation Times.

--- py_client/conversion/convert_json_to_aidm_for_end_to_end_test_tools.py
+++ py_client/conversion/convert_json_to_aidm_for_end_to_end_test_tools.py
@@ -1,9 +1,9 @@
 from typing import List, Union, Dict
 
 from py_client.aidm import StopStatus, UpdateTrainTimesNode, UpdateTrainStopTimesNode, IncomingRoutingEdge, \
-    OutgoingRoutingEdge, CrossingRoutingEdge, UpdateTrainRoute
+    OutgoingRoutingEdge, CrossingRoutingEdge, UpdateTrainRoute, StationEntryOrExit
 from py_client.conversion.algorithm_platform_json_to_aidm_converter import convert
-from py_client.conversion.converter_helpers import convert_keys_to_snake_case, parse_to_datetime
+from py_client.conversion.converter_helpers import convert_keys_to_snake_case, parse_to_datetime, _translate_key
 
 
 def convert_list_of_json_to_update_train_times_node(attribute_dict: dict) -> List[UpdateTrainTimesNode]:
@@ -48,3 +48,23 @@ def create_update_train_route_for_end_to_end_test(object_as_json: dict) -> Updat
             raise TypeError("{0} is not defined as a routing edge".format(edge_as_dict["class"]))
 
     return UpdateTrainRoute(train_id, end_train_path_node_id, converted_routing_edges, start_train_path_node_id)
+
+
+def convert_string_to_stop_status(dict_with_stop_status_as_string) -> StopStatus:
+    stop_status_as_string = _translate_key(dict_with_stop_status_as_string["stop_status_as_string"])
+    for status in StopStatus:
+        if status.name == stop_status_as_string:
+            return StopStatus[stop_status_as_string]
+
+    raise TypeError("{0} is not defined as a Stop Status".format(stop_status_as_string))
+
+
+def convert_string_to_station_entry_or_exit(
+        dict__with_station_entry_or_exit_as_string: Dict[str, str]) -> StationEntryOrExit:
+    station_entry_or_exit_as_string = _translate_key(
+        dict__with_station_entry_or_exit_as_string["station_entry_or_exit_as_string"])
+    for status in StationEntryOrExit:
+        if status.name == station_entry_or_exit_as_string:
+            return StationEntryOrExit[station_entry_or_exit_as_string]
+
+    raise TypeError("{0} is not defined as a EntryOrExit".format(station_entry_or_exit_as_string))

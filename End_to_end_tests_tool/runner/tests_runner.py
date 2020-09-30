9aafa000d838f8c81a708f3ea0ece4d33b55a031
ffu <ffu@sma-partner.com>
2020-09-30 11:32:45 +0200

    VPLAT-7463: Improve runner by verifying that every for each test case, all three paths (examples_file_path, py_call_file_path, expected_file_path) are supplied. raise exception otherwise

--- End_to_end_tests_tool/runner/tests_runner.py
+++ End_to_end_tests_tool/runner/tests_runner.py
@@ -13,6 +13,29 @@ PY_CALL_DIRECTORY = 'py_calls'
 TEST_EXPECTED_DIRECTORY = 'expected'
 
 
+class _PathTripleToDataPerEndToEndTest:
+    __examples_file_path: str
+    __py_call_file_path: str
+    __expected_file_path: str
+
+    def __init__(self, examples_file_path, py_call_file_path, expected_file_path):
+        self.__examples_file_path = examples_file_path
+        self.__py_call_file_path = py_call_file_path
+        self.__expected_file_path = expected_file_path
+
+    @property
+    def examples_file_path(self) -> str:
+        return self.__examples_file_path
+
+    @property
+    def py_call_file_path(self) -> str:
+        return self.__py_call_file_path
+
+    @property
+    def expected_file_path(self) -> str:
+        return self.__expected_file_path
+
+
 def print_all_test_case_statuses_and_messages_in_console_or_file(
         performed_tests: List[EndToEndTestCaseResult],
         file_to_store_results: Optional[Type[open]] = None):
@@ -84,6 +107,44 @@ def run_end_to_end_test_with_running_algorithm_platform_in_mode_start_up_rest_in
         run_end_to_end_tests_with_headless(headless_runner_config, HeadLessRunnerMock(), log_file)
 
 
+def _create_list_of_path_triples_to_data_for_one_end_to_end_test_from_examples_file_paths(
+        headless_runner_config: TestsRunnerConfig) -> List[_PathTripleToDataPerEndToEndTest]:
+    def __extract_path_to_same_filename_in_list(paths_to_file: List[str], filename_to_find: str) -> Optional[str]:
+        for i in range(len(paths_to_file)):
+            if filename_to_find in paths_to_file[i]:
+                return paths_to_file.pop(i)
+        return None
+
+    examples_json_files_with_path = __find_absolute_paths_of_jsons_in_data_subdir(
+        headless_runner_config,
+        TEST_CALL_DIRECTORY)
+    py_call_json_files_with_path = __find_absolute_paths_of_jsons_in_data_subdir(
+        headless_runner_config,
+        PY_CALL_DIRECTORY)
+    headless_runner_config = __find_absolute_paths_of_jsons_in_data_subdir(
+        headless_runner_config,
+        TEST_EXPECTED_DIRECTORY)
+
+    path_triples: List[_PathTripleToDataPerEndToEndTest] = []
+    for examples_file_path in examples_json_files_with_path:
+        file_name = examples_file_path.split("\\")[-1]
+        py_call_file_path = __extract_path_to_same_filename_in_list(py_call_json_files_with_path, file_name)
+        expected_file_path = __extract_path_to_same_filename_in_list(headless_runner_config, file_name)
+
+        if py_call_file_path is None:
+            raise FileNotFoundError(
+                "{0} does not have a corresponding file in {1}"
+                "".format(file_name, os.path.dirname(py_call_json_files_with_path[0])))
+        if expected_file_path is None:
+            raise FileNotFoundError(
+                "{0} does not have a corresponding file in {1}"
+                "".format(file_name, os.path.dirname(headless_runner_config[0])))
+
+        path_triples.append(_PathTripleToDataPerEndToEndTest(examples_file_path, py_call_file_path, expected_file_path))
+
+    return path_triples
+
+
 def run_end_to_end_tests_with_headless(
         headless_runner_config: TestsRunnerConfig,
         running_headless: headless_runner.ViriatoHeadlessRunner,
@@ -93,24 +154,15 @@ def run_end_to_end_tests_with_headless(
     with algorithm_interface_factory.create(base_url) as algorithm_interface:
         running_headless.check_and_wait_for_headless_to_be_ready(algorithm_interface)
 
-        examples_json_files_with_path = __find_absolute_paths_of_jsons_in_data_subdir(
-            headless_runner_config,
-            TEST_CALL_DIRECTORY)
-        py_call_json_files_with_path = __find_absolute_paths_of_jsons_in_data_subdir(
-            headless_runner_config,
-            PY_CALL_DIRECTORY)
-        expected_json_files_with_path = __find_absolute_paths_of_jsons_in_data_subdir(
-            headless_runner_config,
-            TEST_EXPECTED_DIRECTORY)
-
         performed_tests = []
         test_case_reader = TestCaseReader(base_url)
-        for examples_file_path, py_call_file_path, expected_file_path, in \
-                zip(examples_json_files_with_path, py_call_json_files_with_path, expected_json_files_with_path):
+        path_triple_per_test = _create_list_of_path_triples_to_data_for_one_end_to_end_test_from_examples_file_paths(
+            headless_runner_config)
+        for path_triple_for_one_test in path_triple_per_test:
             end_to_end_test_case = test_case_reader.read_test_cases_from_calls_py_calls_and_expected(
-                examples_file_path,
-                py_call_file_path,
-                expected_file_path)
+                path_triple_for_one_test.examples_file_path,
+                path_triple_for_one_test.py_call_file_path,
+                path_triple_for_one_test.expected_file_path)
             performed_tests.append(execute_test_case(algorithm_interface, end_to_end_test_case))
 
     print_all_test_case_statuses_and_messages_in_console_or_file(performed_tests)

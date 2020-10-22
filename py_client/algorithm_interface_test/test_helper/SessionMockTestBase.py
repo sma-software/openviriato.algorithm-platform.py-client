434fdc681c5b45992c30426fed2880ff16eb2e9d
ffu <ffu@sma-partner.com>
2020-10-22 21:10:28 +0200

    NO-ISSUE: Change SessionMockTestBase to allow more efficient/transparent development of Unittests for algorithm_interface

--- py_client/algorithm_interface_test/test_helper/SessionMockTestBase.py
+++ py_client/algorithm_interface_test/test_helper/SessionMockTestBase.py
@@ -1,10 +1,24 @@
 class SessionMockTestBase:
-    __last_body: dict
-    __last_request: str
+    _last_request: str
+    _last_parameters: dict
+    _last_body: dict
 
     def __init__(self):
-        self.__last_request = str()
-        self.__last_body = dict()
+        self._last_request = str()
+        self._last_parameters = dict()
+        self._last_body = dict()
+
+    @property
+    def last_request(self) -> str:
+        return self._last_request
+
+    @property
+    def last_parameters(self) -> dict:
+        return self._last_parameters
+
+    @property
+    def last_body(self) -> dict:
+        return self._last_body
 
     def close(self):
         pass

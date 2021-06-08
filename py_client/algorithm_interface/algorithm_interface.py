714b5bcf374bce3acf0e75b07e1ef38e4a486b85
ffu <ffu@sma-partner.com>
2021-06-08 12:06:00 +0200

    VPLAT-8832: Add get_preferred_routes_through_node method to interface

--- py_client/algorithm_interface/algorithm_interface.py
+++ py_client/algorithm_interface/algorithm_interface.py
@@ -53,6 +53,25 @@ class AlgorithmInterface:
             response_list
         )
 
+    def get_preferred_routes_through_node(
+            self,
+            node_id: int,
+            start_section_track_id: Optional[int],
+            end_section_track_id: Optional[int]
+    ) -> List[RoutingEdgePair]:
+        url_to_resource = 'nodes/{0}/preferred-routes'.format(node_id)
+        request_parameters = dict(
+            startSectionTrackID=start_section_track_id,
+            endSectionTrackID=end_section_track_id,
+            endInNode=False,
+            startInNode=False,
+        )
+        response_list = self.__communication_layer.do_get_request(url_to_resource, request_parameters)
+        return algorithm_platform_json_to_aidm_converter.convert_list(
+            algorithm_platform_json_to_aidm_converter.convert_to_routing_edge_pair,
+            response_list
+        )
+
     def get_all_section_tracks(self) -> List[AlgorithmSectionTrack]:
         url_to_resource = 'section-tracks'
         response_list = self.__communication_layer.do_get_request(url_to_resource)

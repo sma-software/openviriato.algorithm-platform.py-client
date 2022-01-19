ba86312f25c753bd5dbd39700fa660a73010f7f9
cbe <cbe@sma-partner.com>
2022-01-19 11:44:08 +0100

    VPLAT-8570: Refactoring. Iterate to each attribute to convert to the correct type.

--- py_client/conversion/json_to_aidm_converter.py
+++ py_client/conversion/json_to_aidm_converter.py
@@ -1,14 +1,32 @@
-from typing import Type, List
-from py_client.conversion.json_to_aidm_processors import AidmWithPrimitivesOrListOfPrimitivesProcessor, JsonToAidmProcessor
+from typing import Type, List, get_type_hints
+from py_client.conversion.json_to_aidm_processors import JsonToAidmProcessor
 from py_client.communication.response_processing import AlgorithmPlatformConversionError
 from py_client.aidm.aidm_base_classes import _HasID
+from py_client.conversion.converter_helpers import convert_keys_to_snake_case
 
 class JsonToAidmConverter:
+    __processors: List[JsonToAidmProcessor]
+
+    def __init__(self):
+        self.__processors = []
 
     def process_json_to_aidm(self, attribute_dict: dict, aidm_class: Type[object]) -> _HasID:
+        state = attribute_dict
+
+        object_attribute_and_attribute_type = get_type_hints(aidm_class)
+        for attribute_name_with_class_name, attribute_type in object_attribute_and_attribute_type.items():
+            attribute_name = attribute_name_with_class_name.split("__")[-1]
+
+            for processor in self.__processors:
+                if (processor.is_applicable(self.__processors)):
+                    state = processor.process_attribute_dict(state[attribute_name])
         try:
-            return AidmWithPrimitivesOrListOfPrimitivesProcessor().process_attribute_dict(attribute_dict, aidm_class)
+            return self.transform_processed_dict_to_aidm(aidm_class, state)
         except TypeError as e:
             raise AlgorithmPlatformConversionError(
                 "Could not populate AIDM object, AIDM class {} is unknown.".format(aidm_class),
                 e)
+
+    def transform_processed_dict_to_aidm(self, aidm_class, state):
+        snake_case_attribute_dict = convert_keys_to_snake_case(state)
+        return aidm_class(**snake_case_attribute_dict)
--- py_client/conversion/json_to_aidm_processors.py
+++ py_client/conversion/json_to_aidm_processors.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Type, Tuple
 from py_client.aidm.aidm_base_classes import _HasID
-from py_client.conversion.converter_helpers import convert_keys_to_snake_case
 from typing import get_type_hints, Optional, List
 
 class JsonToAidmProcessor:
@@ -12,13 +11,3 @@ class JsonToAidmProcessor:
     @abstractmethod
     def process_attribute_dict(self, attribute_dictionary: dict, aidm_class: Type[object]) -> None:
         pass
-
-class AidmWithPrimitivesOrListOfPrimitivesProcessor(JsonToAidmProcessor):
-    def is_applicable(self, aidm_class: Type[object]) -> bool:
-        attributeTypes = [attrType for _, attrType in get_type_hints(aidm_class).items()]
-        builtInTypes = [str, int, bool, Optional[str], Optional[int], Optional[bool], List[str], List[int], List[bool]]
-        return all([a in builtInTypes for a in attributeTypes])
-
-    def process_attribute_dict(self, attribute_dictionary: dict, aidm_class: Type[_HasID]) -> _HasID:
-        snake_case_attribute_dict = convert_keys_to_snake_case(attribute_dictionary)
-        return aidm_class(**snake_case_attribute_dict)

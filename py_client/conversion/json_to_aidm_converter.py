2b5fbdf928a6f7635203a9303cc9d9ca97d505f7
cbe <cbe@sma-partner.com>
2022-01-19 11:18:49 +0100

    VPLAT-8570: Refactoring of the converter and processor to prepare for extension.

--- py_client/conversion/json_to_aidm_converter.py
+++ py_client/conversion/json_to_aidm_converter.py
@@ -1,21 +1,14 @@
 from typing import Type, List
 from py_client.conversion.json_to_aidm_processors import AidmWithPrimitivesOrListOfPrimitivesProcessor, JsonToAidmProcessor
 from py_client.communication.response_processing import AlgorithmPlatformConversionError
+from py_client.aidm.aidm_base_classes import _HasID
 
 class JsonToAidmConverter:
-    __processors: List[JsonToAidmProcessor]
 
-    def __init__(self):
-        self.__processors = [AidmWithPrimitivesOrListOfPrimitivesProcessor()]
-
-    def process_json_to_aidm(self, attribute_dict: dict, aidm_class: Type[object]) -> object:
-        for processor in self.__processors:
-            if processor.is_applicable(aidm_class):
-                try:
-                    return processor.process_attribute_dict(aidm_class, attribute_dict)
-                except TypeError as error_instance:
-                    raise AlgorithmPlatformConversionError(
-                        "Could not populate AIDM object, client AIDM class deviates from server AIDM class",
-                        error_instance)
-        raise AlgorithmPlatformConversionError(
-                        "Could not populate AIDM object, AIDM class {} is unknown.".format(aidm_class), None)
+    def process_json_to_aidm(self, attribute_dict: dict, aidm_class: Type[object]) -> _HasID:
+        try:
+            return AidmWithPrimitivesOrListOfPrimitivesProcessor().process_attribute_dict(attribute_dict, aidm_class)
+        except TypeError as e:
+            raise AlgorithmPlatformConversionError(
+                "Could not populate AIDM object, AIDM class {} is unknown.".format(aidm_class),
+                e)
--- py_client/conversion/json_to_aidm_processors.py
+++ py_client/conversion/json_to_aidm_processors.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Type, Tuple
-from py_client.aidm import AlgorithmSectionTrack
+from py_client.aidm.aidm_base_classes import _HasID
 from py_client.conversion.converter_helpers import convert_keys_to_snake_case
 from typing import get_type_hints, Optional, List
 
@@ -10,7 +10,7 @@ class JsonToAidmProcessor:
         pass
 
     @abstractmethod
-    def process_attribute_dict(cls, aidm_class: Type[object], attribute_dictionary: dict) -> None:
+    def process_attribute_dict(cls, attribute_dictionary: dict, aidm_class: Type[object]) -> None:
         pass
 
 class AidmWithPrimitivesOrListOfPrimitivesProcessor(JsonToAidmProcessor):
@@ -19,6 +19,6 @@ class AidmWithPrimitivesOrListOfPrimitivesProcessor(JsonToAidmProcessor):
         builtInTypes = [str, int, bool, Optional[str], Optional[int], Optional[bool], List[str], List[int], List[bool]]
         return all([a in builtInTypes for a in attributeTypes])
 
-    def process_attribute_dict(cls, aidm_class: Type[object], attribute_dictionary: dict) -> AlgorithmSectionTrack:
+    def process_attribute_dict(cls, attribute_dictionary: dict, aidm_class: Type[_HasID]) -> _HasID:
         snake_case_attribute_dict = convert_keys_to_snake_case(attribute_dictionary)
         return aidm_class(**snake_case_attribute_dict)

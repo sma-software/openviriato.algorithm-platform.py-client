49d5536398d2d64810540d7f8b2a630e2afbc3d2
cbe <cbe@sma-partner.com>
2022-02-04 12:49:32 +0100

    VPLAT-9453: Implement delete_train.

--- py_client/algorithm_interface/algorithm_interface.py
+++ py_client/algorithm_interface/algorithm_interface.py
@@ -252,6 +252,10 @@ class AlgorithmInterface:
         url_to_resource = "trains/{0}:cancel".format(train_id)
         self.__communication_layer.do_delete_request(url_to_resource)
 
+    def delete_train(self, train_id: int) -> None:
+        url_to_resource = "trains/{0}:delete".format(train_id)
+        self.__communication_layer.do_delete_request(url_to_resource)
+
     def update_movement_type(self, movement_type_id: int, train_id: int, from_train_path_node_id: int, to_train_path_node_id: int) -> AlgorithmTrain:
         url_to_resource = "trains/{0}/train-path-nodes:update-movement-type".format(train_id)
         put_body_dict = dict(movementTypeId = movement_type_id,
---
+++ py_client/algorithm_interface_test/trains/test_delete_train.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest import mock
+
+import py_client.algorithm_interface_test.test_helper.SessionMockFactory as SessionMockFactory
+from py_client.algorithm_interface import algorithm_interface_factory
+from py_client.algorithm_interface_test.test_helper.SessionMockTestBase import \
+    get_api_url, \
+    SessionMockTestBase
+
+
+class TestDeleteTrain(unittest.TestCase):
+    class DeleteTrainTestMockSession(SessionMockTestBase):
+        def delete(self, request, json):
+            self._last_body = json
+            self._last_request = request
+            return SessionMockFactory.create_response_mock("8124", 200)
+
+    @mock.patch('requests.Session', side_effect=DeleteTrainTestMockSession)
+    def setUp(self, mocked_get_obj):
+        self.interface_to_viriato = algorithm_interface_factory.create(get_api_url())
+
+    @mock.patch('requests.Session', side_effect=DeleteTrainTestMockSession)
+    def test_delete_train_request(self, mocked_get_obj):
+        train_id = 8124
+        self.interface_to_viriato.delete_train(train_id)
+
+        session_obj = self.interface_to_viriato._AlgorithmInterface__communication_layer.currentSession
+        self.assertEqual(session_obj.last_request, get_api_url() + "/trains/" + str(train_id) + ":delete")
+        self.assertDictEqual(session_obj.last_body, {})
+
+    @mock.patch('requests.Session', side_effect=DeleteTrainTestMockSession)
+    def test_cancel_train_response(self, mocked_get_obj):
+        train_id = 8124
+
+        # test and validate
+        self.assertIsNone(self.interface_to_viriato.delete_train(train_id))
+
+    @mock.patch('requests.Session', side_effect=DeleteTrainTestMockSession)
+    def tearDown(self, mocked_get_obj) -> None:
+        self.interface_to_viriato.__exit__(None, None, None)
